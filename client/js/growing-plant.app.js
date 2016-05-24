ae9ba8786e789b699d8806c06b9d307f915f5230
machidoine <benjamin.guilloteau@gmail.com>
2016-05-25 01:02:21 +0200

    add angular JS

--- client/js/growing-plant.app.js
+++ client/js/growing-plant.app.js
@@ -1,35 +1,76 @@
 console.log("hello");
 angular.module("plant", [])
 .controller("GrowingPlantController", function($scope, $element) {
-    $($element).on('click','.grid-element',function(){
-		var index = $(this).data("index");
-    	$scope.send(index);
-    	console.log('click element', index);
-    })
+	$scope.grid = [];
+	$scope.menuModel = {currentItem : {}, items : [{type : 'seed'}, {type : 'tutor'}, {type : 'fork'}]}
+    var socket = io.connect();  
 
-    var socket = io.connect();
-    
-    $scope.playGrid = [];  
-    
-    socket.on('connect', function() {
-
-        $scope.$apply();
-    });
-
-    socket.on('myConnect', function(grid) {
-		$scope.playGrid = grid;
-        $scope.$apply();
+    socket.on('myConnect', function(points) {
+    	$scope.grid = points;
+    	$scope.$apply();
+		//drawPoints(points);
     });
     
-    socket.on('gridElementReceive', function(grid) {
-        //console.log($scope.playGrid);
-        $scope.playGrid = grid;
-        //console.log($scope.playGrid);
-        $scope.$apply();
+    socket.on('gridElementReceive', function(points) {
+    	$scope.grid = points;
+    	$scope.$apply();
+        console.log("Promis j'ai cliqué");
+       //drawPoints(points);
     });
     
     $scope.send = function send(gridIndex) {
-        socket.emit('addGridElement', gridIndex);
+    	if($scope.menuModel.currentItem.type) {
+    		gridIndex.type = $scope.menuModel.currentItem.type;
+    		console.log("sending...", gridIndex);
+        	socket.emit('addGridElement', gridIndex);	
+    	}
+    	
     }
 
-})
+ })
+ .directive("gpGrid", function() {
+ 	return {
+ 		templateUrl : 'templates/grid.html',
+ 		scope : {grid : "=", currentItemSelected : "=", clickOnGrid:"&"},
+ 		restrict:"E",
+ 		controller : function($scope, $element) {
+			$($element).on('click',function(e){
+				var posX = e.pageX - $(this).offset().left;
+				var posY = e.pageY - $(this).offset().top;
+
+				var indexX =  Math.floor(posX / 15); 
+				var indexY = Math.floor(posY / 15); 
+				
+				$scope.clickOnGrid()({"x":indexX,"y":indexY});
+				console.log("click event");
+			})
+
+			$scope.placeholderGridItem = {x:0,y:0,type:""};
+
+			$scope.onMouseMove = function(e){
+				var posX = e.pageX - $(e.currentTarget).offset().left;
+				var posY = e.pageY - $(e.currentTarget).offset().top;
+
+				var indexX =  Math.floor(posX / 15); 
+				var indexY = Math.floor(posY / 15);
+
+				$scope.placeholderGridItem = {
+					x:indexX,
+					y:indexY,
+					type:$scope.currentItemSelected.type
+				}
+			}
+ 		}
+ 	};
+ })
+ .directive("gpMenuItems", function() {
+ 	console.log("tot");
+ 	return {
+ 		templateUrl : 'templates/menu-items.html',
+ 		restrict : 'E',
+ 		scope : {menuModel : "="},
+ 		controller : function($scope, $element) {
+			
+ 		}
+ 	}
+ })
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict'
 //
 // # SimpleServer
 //
@@ -24,17 +25,48 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 
 var sockets = [];
 var players = {};
-var color = ["blue", "red", "black"]
-var points = [
-{"x":0,"y":0,"type":"seed"}
-];
+var color = ["blue", "red", "black"];
+
+var Grid = class {
+    constructor(width, height, initialPoints) {
+        this.width = width;
+        this.height = height;
+        this._points = initialPoints;
+    }
+
+    get points() {
+        return this._points;
+    }
+
+    addPoints(point) {
+        var index = this._points.findIndex(function(element){
+            return element.x === point.x && element.y === point.y;
+        });
+
+        if(index < 0) {
+            this._points.push(point);    
+        } else {
+            this._points[index] = point;
+        }
+    }
+
+}
+
+var initialPoints = [];
+for(let i = 0; i < 50; i++) {
+    for(let j = 0; j < 49; j++) {
+        initialPoints.push({"x":i, "y":j, "type":"seed"});
+    }
+}
+initialPoints = [{"x":0, "y":0, "type":"seed"}]
+var grid = new Grid(50, 50, initialPoints);
 
 io.on('connection', function(socket) {
     sockets.push(socket);
     players[socket.id] = {};
     players[socket.id].color = color[Math.floor(Math.random() * 3)];
 
-    socket.emit('myConnect', points);
+    socket.emit('myConnect', grid.points);
     
     socket.on('disconnect', function() {
         sockets.splice(sockets.indexOf(socket), 1);
@@ -44,9 +76,8 @@ io.on('connection', function(socket) {
         console.log(socket);
         gridElement.color = players[socket.id].color;
         console.log(players);
-        points.push(gridElement);
-        console.log(points);
-        broadcast('gridElementReceive', points);
+        grid.addPoints(gridElement);
+        broadcast('gridElementReceive', grid.points);
     
     });
 });
