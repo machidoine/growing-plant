f1efb7ed209bbe76dc1f4f2e227c004e15b580ad
bguilloteau <benjamin.guilloteau@gmail.com>
2016-10-07 01:13:56 +0200

    implement front - possibility to choose direction for seed and stem

---
+++ src/client/js/constants.js
@@ -0,0 +1,15 @@
+'use strict'
+/**
+ * Created by bguilloteau on 06/10/16.
+ */
+
+class Constants {
+    static get directions() {
+      return  {
+          'up': 0,
+          'down': 180,
+          'right': 90,
+          'left': -90
+      };
+    }
+}
--- src/client/js/growing-plant.phaser.app.js
+++ src/client/js/growing-plant.phaser.app.js
@@ -14,65 +14,8 @@ $(document).ready(function () {
 
     var socket;
     var game;
-    var plantMap;
-    var directions = {
-        'up': 0,
-        'down': 180,
-        'right': 90,
-        'left': -90
-    }
-
-    var PlantMap = class {
-        constructor(game) {
-            this.game = game;
-            this.group = game.add.group();
-            this._sprites = [];
-        }
-
-        add(gardenElement) {
-            var sprite = this.group.create(0, 0, 'plan-textures', gardenElement.type + '-' + gardenElement.team);
-            /*sprite.rotation = directions[plan.direction];
-             sprite.pivot.x = sprite.x + 12.5;
-             sprite.pivot.y = sprite.y + 12.5;
-             sprite.anchor.setTo(0,0);
-             sprite.pivot.x = sprite.x + 12.5;
-             sprite.pivot.y = sprite.y + 12.5;*/
-            sprite.anchor.setTo(0.5, 0.5);
-            sprite.angle = directions[gardenElement.direction];
-            sprite.x = sprite.width * gardenElement.position.x + 12.5;
-            sprite.y = sprite.height * gardenElement.position.y + 12.5;
-        }
-
-        spriteInputListener(e) {
-            console.log('clicked');
-            console.log(e);
-        }
-
-        clear() {
-            this.group.removeAll();
-        }
-
-        addAll(gardenElements) {
-            var me = this;
-            gardenElements.forEach(function (gardenElement) {
-                me.add(gardenElement);
-            });
-        }
-
-        replaceBy(gardenElements) {
-            this.clear();
-            this.addAll(gardenElements);
-            // this.group.callAll('events.onInputDown.add', 'events.onInputDown', this.spriteInputListener, this);
-            // this.group.setAll('inputEnabled', true);
-        }
-
-        getCellByType(type) {
-            return this._sprites.filter(function (e) {
-                return e.type == type;
-            });
-        }
-
-    }
+    var gardenElementMap;
+    var uiMap;
 
     function preload() {
         game.load.atlas('plant-textures', 'assets/growing-plant-spritesheet.png', 'assets/growing-plant-spritesheet.json');
@@ -81,61 +24,24 @@ $(document).ready(function () {
     function create() {
         var graphics = game.add.graphics(0, 0);
 
+        gardenElementMap = new GardenElementMap(game);
 
-        plantMap = new PlantMap(game);
 
         socket = io.connect();
+        uiMap = new UIMap(game, socket);
+
         socket.on('myConnect', function (gridReceived) {
-            plantMap.replaceBy(gridReceived);
+            gardenElementMap.replaceBy(gridReceived);
         });
         socket.on('gridElementReceive', function (gridReceived) {
             $('.debug').html(JSON.stringify(gridReceived, null, 4).replace(/(?:\r\n|\r|\n)/g, '<br />'));
-            plantMap.replaceBy(gridReceived);
+            gardenElementMap.replaceBy(gridReceived);
         });
 
         game.stage.backgroundColor = '#befe9e';
-        // plantMap.addPlant({x: 0, y: 0, type: 'seed'});    
 
-        game.input.addMoveCallback(clickOnGrid, this);
     }
 
-    function clickOnGrid() {
-
-        if (game.input.activePointer.leftButton.isDown) {
-            //console.log('clicked');
-            var mouseX = game.input.activePointer.worldX;
-            var mouseY = game.input.activePointer.worldY;
-
-            var x = game.math.snapToFloor(mouseX, 25) / 25;
-            var y = game.math.snapToFloor(mouseY, 25) / 25;
-
-            var direction = ["up", "down", "right", "left"][Math.floor(Math.random() * 4)];
-
-            socket.emit('addGridElement', {position: {x: x, y: y}, type: 'seed', direction: direction});
-
-            //$.getJSON('http://localhost:3000/data.json',{}, function(data){
-            //plantMap.replaceBy(data);
-            //});
-
-        }
-        if (game.input.activePointer.rightButton.isDown) {
-            //console.log('clicked');
-            var mouseX = game.input.activePointer.worldX;
-            var mouseY = game.input.activePointer.worldY;
-
-            var x = game.math.snapToFloor(mouseX, 25) / 25;
-            var y = game.math.snapToFloor(mouseY, 25) / 25;
-
-            var direction = ["up", "down", "right", "left"][Math.floor(Math.random() * 4)];
-
-            socket.emit('addGridElement', {position: {x: x, y: y}, type: 'stem', direction: direction});
-
-            //$.getJSON('http://localhost:3000/data.json',{}, function(data){
-            //plantMap.replaceBy(data);
-            //});
-
-        }
-    }
 
     function update() {
     }
